9db379a2dd8cc13a763376e8ad388cfd173267cd
Andre Rezende <meirezende@hotmail.com>
2017-04-06 09:00:07 -0300

    New spring boot validation example.

---
+++ Agenda/README.md
@@ -0,0 +1,26 @@
+These is a Spring boot example. The project must start an tomcat and validate fields before save them into HSQLDB.
+
+# Libraries and tools
+- Spring boot
+- Spring MVC
+- Jersey
+- Bootstrap 
+- jQUery
+- Maven
+
+# Database
+- HSQLDB
+
+#Container
+- TomEE
+
+# IDE
+- Eclipse
+
+# Steps to install
+- 1 - Unzip file or checkout from github.
+- 2 - Install all maven dependencies (On Eclipse, right mouse click on Project > Maven > Update Project)
+- 3 - Check if all dependencies were downloaded. 
+- 4 - Run Application.java class as Java Application.
+
+- If everything is well, then open the follow URL in your favorite browser: http://localhost:8080
---
+++ Agenda/src/main/java/br/com/rezende/springboot/example/agenda/controller/WebController.java
@@ -0,0 +1,37 @@
+package br.com.rezende.springboot.example.agenda.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import br.com.rezende.springboot.example.agenda.model.PersonForm;
+
+
+@Controller
+public class WebController extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm) {
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "index";
+        }
+
+        return "redirect:/results";
+    }
+}
